8cb2f903c18dd749a83598c4f6799a7227558199
JaimeGM96 <jgalmat@gmail.com>
2023-05-23 10:54:01 +0200

    feat(asgi.py): Change imports

--- dashboard/netmalies/asgi.py
+++ dashboard/netmalies/asgi.py
@@ -3,7 +3,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import mainboard.routing
+from mainboard.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'netmalies.settings')
 
@@ -11,7 +11,7 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     'websocket': AuthMiddlewareStack(
         URLRouter(
-            mainboard.routing.websocket_urlpatterns
+            websocket_urlpatterns
         )
     )
 })
