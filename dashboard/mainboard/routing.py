f4644493ce0f0d0de2e10432bd9ab38f8c8b0a58
JaimeGM96 <jgalmat@gmail.com>
2023-05-23 10:53:10 +0200

    feat(routing.py): Change route name

--- dashboard/mainboard/routing.py
+++ dashboard/mainboard/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from mainboard import consumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/127.0.0.1:8765', consumer.SensorConsumer.as_asgi()),
+    re_path(r'ws/monitoring/', consumer.SensorConsumer.as_asgi()),
 ]
