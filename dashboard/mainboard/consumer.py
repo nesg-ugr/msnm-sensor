d0afe774575d0c573bd15af05d84066a1777adce
JaimeGM96 <jgalmat@gmail.com>
2023-07-05 18:57:31 +0200

    feat(consumer.py): Add functionality to handle new parameters received from sensor

--- dashboard/mainboard/consumer.py
+++ dashboard/mainboard/consumer.py
@@ -6,34 +6,42 @@ class SensorConsumer(AsyncWebsocketConsumer):
     _ts = []
     _qst = []
     _dst = []
-    channel_layer = get_channel_layer()
+    _uclq = []
+    _ucld = []
     
     async def connect(self):
+        self.channel_layer.group_add("sensors", self.channel_name)
         await self.accept()
+
+    async def disconnect(self, close_code):
+        self.channel_layer.group_discard("sensors", self.channel_name)
+        await self.close()
 
     async def receive(self, text_data):
         statistics = json.loads(text_data)
-        if len(self._ts) > 10:
+        if len(self._ts) > 20:
             self._ts.pop(0)
-            self._ts.append(statistics[0]['ts'])
-        else:
-            self._ts.append(statistics[0]['ts'])
-
-        if len(self._qst) > 10:
             self._qst.pop(0)
-            self._qst.append(statistics[0]['Qst'])
-        else:
-            self._qst.append(statistics[0]['Qst'])
-        
-        if len(self._dst) > 10:
             self._dst.pop(0)
+            self._uclq.pop(0)
+            self._ucld.pop(0)
+            self._ts.append(statistics[0]['ts'])
+            self._qst.append(statistics[0]['Qst'])
             self._dst.append(statistics[0]['Dst'])
+            self._uclq.append(statistics[0]['UCLq'])
+            self._ucld.append(statistics[0]['UCLd'])
         else:
+            self._ts.append(statistics[0]['ts'])
+            self._qst.append(statistics[0]['Qst'])
             self._dst.append(statistics[0]['Dst'])
+            self._uclq.append(statistics[0]['UCLq'])
+            self._ucld.append(statistics[0]['UCLd'])
 
         print(self._ts)
         print(self._qst)
         print(self._dst)
+        print(self._uclq)
+        print(self._ucld)
         await self.send(text_data="Information received")
 
     async def send_data(self, data):
