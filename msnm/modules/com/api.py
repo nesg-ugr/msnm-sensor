000b32bf44f3063a7205d01d92d2120070d3c5cc
JaimeGM96 <jgalmat@gmail.com>
2023-07-20 20:02:54 +0200

    feat(api.py): Add set config in Configure class

--- msnm/modules/com/api.py
+++ msnm/modules/com/api.py
@@ -25,13 +25,17 @@ async def sendConfig(config: Request):
     config = await config.json()
 
     # Set new configuration params into the singleton pattern
-    sensor_config_params.config_params = config
-
+    sensor_config_params.get_config()['Sensor']['lv'] = config['Sensor']['lv']
+    sensor_config_params.get_config()['Sensor']['prep'] = config['Sensor']['prep']
+    sensor_config_params.get_config()['Sensor']['phase'] = config['Sensor']['phase']
+    sensor_config_params.get_config()['Sensor']['dynamiCalibration']['B'] = config['Sensor']['dynamiCalibration']['B']
+    sensor_config_params.get_config()['Sensor']['dynamiCalibration']['lambda'] = config['Sensor']['dynamiCalibration']['lambda']
+    sensor_config_params.get_config()['Sensor']['dynamiCalibration']['enabled'] = config['Sensor']['dynamiCalibration']['enabled']
 
     return {
         "status": "SUCCESS",
         "message": "Config receive successfully",
-        "data": sensor_config_params.get_config(),
+        "data": sensor_config_params.get_config()['Sensor'],
     }
 
 if __name__ == '__main__':
